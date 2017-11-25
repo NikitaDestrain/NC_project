671016393cac4d1f88233d57ac637379501aa53d
AndreyVic <33463480+AndreyVic@users.noreply.github.com>
2017-11-25 16:23:59 +0300

    Добавил класс диалогового окна
    Диалоговое окно показывает о неверном заполнении полей TaskWindow

---
+++ src/GUI/TaskWindow/ErrDialog.java
@@ -0,0 +1,31 @@
+package GUI.TaskWindow;
+import TaskWindow;
+import java.awt.*;
+
+import javax.swing.*;
+
+public class ErrDialog extends JDialog {
+
+    private JFrame owner;
+    private String s_dialog;
+
+    public ErrDialog(TaskWindow owner, String s_dialog) {
+        super(owner, s_dialog, true);
+        this.s_dialog = s_dialog;
+        this.owner = owner;
+        setLocationRelativeTo(null);
+        setSize(260, 160);
+        initComponents();
+        setVisible(true); // отобразить диалог
+
+    }
+
+    private void initComponents() {
+
+        JPanel panel2 = new JPanel();
+
+        panel2.add(new JLabel(s_dialog));
+        add(panel2, BorderLayout.NORTH);
+
+    }
+}
