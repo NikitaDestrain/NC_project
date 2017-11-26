5f0955bc08b0a5d61716308bcea3b5fb485000cb
NikitaDestrain <destra1n1337@gmail.com>
2017-11-26 23:46:24 +0300

    Fix Model
    Fix Task and Journal

--- src/model/Journal.java
+++ src/model/Journal.java
@@ -5,47 +5,51 @@ import java.util.*;
 
 public class Journal implements Serializable {
     private List<Task> tasks;
-    private IDGenerator generator = new IDGenerator();//todo IDGenerator must be single in whole system
 
     public Journal(){
         tasks = new LinkedList<>();
-        generator.setTaskList(tasks);
     }
 
     public Journal(List<Task> tasks) {
         this.tasks = tasks;
-        generator.setTaskList(tasks);
-    }
-
-    //todo not needed?
-    public IDGenerator getGenerator() {
-        return generator;
     }
 
     public void addTask(Task task) {
-        task.setId(generator.createId());
-        tasks.add(task);
-    }
-
-    public void addTask(String name, TaskStatus status, String description, Date notificationDate, Date plannedDate) {
-        Task task = new Task(name, status, description, notificationDate, plannedDate);//todo tasks creation is not journal responsibility FACTORY TASKS
-        task.setId(generator.createId());
         tasks.add(task);
         tasks.sort(new TaskComparator());
     }
 
+    /*
+    public void addTask(String name, TaskStatus status, String description, Date notificationDate, Date plannedDate, int id) {
+        Task task = new Task(name, status, description, notificationDate, plannedDate);//todo tasks creation is not journal responsibility FACTORY TASKS
+        tasks.add(task);
+        tasks.sort(new TaskComparator());
+    }*/
+
     public void removeTask(int id) {
         tasks.remove(id);
-        //generator.updateID(id);
     }
 
     public void setTask(int id, Task task) {
-        tasks.set(id,task);
+        tasks.set(id, task);
         //todo simple replace? DONE
+    }
+
+    public Task getTask(int id) {
+        return tasks.get(id);
     }
 
     public List<Task> getTasks() {
         return Collections.unmodifiableList(tasks);//todo unmodifiable list DONE
+    }
+
+    public int getMaxId(){
+        int res = 0;
+        for (int i = 0; i < tasks.size(); i++) {
+            if(tasks.get(i).getId() > res)
+                res = tasks.get(i).getId();
+        }
+        return res;
     }
 
     @Override
