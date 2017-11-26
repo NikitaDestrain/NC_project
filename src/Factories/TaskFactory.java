7ad09ddb0c7cf7f6037e9d1fee8a9dc12d7e9dc4
AndreyVic <33463480+AndreyVic@users.noreply.github.com>
2017-11-26 23:41:44 +0300

    Добавил возможную реализацию TaskFactory создающую объект Task

---
+++ src/Factories/TaskFactory.java
@@ -0,0 +1,22 @@
+package Factories;
+
+
+import model.Task;
+import model.TaskStatus;
+
+import java.util.Date;
+
+public class TaskFactory  //фабрика Task только она должна явно создавать объект Task
+
+{
+   //реализовать тут проверку корректности данных?
+    //Так как знаем что задача описыватеся 1 классом ->не абстрактная
+   public static Task createTask (String name, TaskStatus status, String description, Date notificationDate, Date plannedDate)
+   {
+
+       return new Task(name, status, description, notificationDate, plannedDate);/// назначить ли id тут? или оставить определение id классу Journal
+
+   }
+//... еще какие-нибудь перегруженные методы
+
+}
