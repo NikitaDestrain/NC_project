54e5264671e4bf98ee359b560eb2a1104e56e4c1
NikitaDestrain <destra1n1337@gmail.com>
2017-11-29 02:31:45 +0300

    Исправлены методы добавления

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,5 @@
 package controller;
 
-import com.sun.xml.internal.bind.v2.model.core.ID;
 import model.Journal;
 import model.Task;
 import model.TaskStatus;
@@ -12,7 +11,6 @@ public class Controller {
     private Journal journal;
     private Notifier notifier;
     private IDGenerator idGenerator;
-    private static TaskStatus DEFAULT_STATUS = TaskStatus.Planned;
     private static Controller instance;
 
     private Controller() {
@@ -36,15 +34,8 @@ public class Controller {
     }
 
     //создается таск вместе с оповещением
-    public void addTask(String name, String description, int day, int month, int year, int hour, int minute, int cntHoursForNotification, int cntMinutesForNotification) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, day, hour, minute);
-        Date d1 = calendar.getTime(); //plannedDate
-        calendar.set(year, month, day, hour - cntHoursForNotification, minute - cntMinutesForNotification);
-        Date d2 = calendar.getTime(); //notifDate
-        if(checkDate(d2)) {
-            Task task = new Task(name, DEFAULT_STATUS, description, d2, d1);
-            task.setId(idGenerator.createId());
+    public void addTask(Task task) {
+        if(checkDate(task.getNotificationDate())) {
             journal.addTask(task);
             notifier.addNotification(task);
         }
@@ -54,13 +45,13 @@ public class Controller {
 
     //удаляется таск(????) и оповещение
     public void removeTask(int id){
-        notifier.cancelNotification(journal.getTask(id).getId());
+        notifier.cancelNotification(id);
         journal.removeTask(id);
     }
 
     //отменяется оповещение
     public void cancelNotification(int id){
-        notifier.cancelNotification(journal.getTask(id).getId());
+        notifier.cancelNotification(id);
         journal.getTask(id).setStatus(TaskStatus.Cancelled);
     }
     //todo map create
@@ -68,61 +59,13 @@ public class Controller {
     public void updateNotification(int id){
         Task task = journal.getTask(id);
         task.setStatus(TaskStatus.Rescheduled);
-        notifier.editNotification(journal.getTask(id).getId(), task);
+        notifier.editNotification(id, task);
         journal.getTask(id).setStatus(TaskStatus.Rescheduled);
     }
 
     //изменение таски полностью !!!edit rename
-    public void setTask(int id, String name, String description, int day, int month, int year, int hour, int minute, int cntHoursForNotification, int cntMinutesForNotification){
-        //notifier.cancelNotification(journal.getTask(id).getId());
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, day, hour, minute);
-        Date d1 = calendar.getTime(); //plannedDate
-        calendar.set(year, month, day, hour - cntHoursForNotification, minute - cntMinutesForNotification);
-        Date d2 = calendar.getTime(); //notifDate
-        if(checkDate(d2)) {
-            Task task = new Task(name, DEFAULT_STATUS, description, d2, d1);//todo do not create
-            notifier.cancelNotification(journal.getTask(id).getId());
-            task.setId(idGenerator.createId());
-            journal.setTask(id, task);
-            notifier.addNotification(task);
-        }
-        else
-            System.out.println("Invalid Date!!!"); //сообщение в форму о неверной дате
-
+    public void editTask(int id, Task task){
+        journal.setTask(id, task);
+        notifier.editNotification(id, task);
     }
-
-    //probably we will use it later...
-/*
-    public Date parseStringToDate(String date) {
-        //todo private. Or better - move it to Utils class
-        Date parsedDate = null;
-        try {
-            parsedDate = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH).parse(date);
-        } catch (ParseException e) {
-            System.out.println("Некорректная дата"); // тут отправка сообщения в форму
-        }
-        return parsedDate;
-    }
-
-    private boolean isTwoDatesNotNull(Date date1, Date date2) {
-        //todo --//--
-        return date1 != null && date2 != null;
-    }
-
-    public void setTask(int id, String name, TaskStatus status, String description, String notificationDate, String plannedDate) {
-        Date notification = parseStringToDate(notificationDate);
-        Date planned = parseStringToDate(plannedDate);
-        if (isTwoDatesNotNull(notification, planned)) {
-            journal.setTask(id, new Task(name, status, description, notification, planned));
-            //notifier edit
-        }
-        else
-            System.out.println("Дата оповещения позже запланированной");
-        //todo else?..
-    }
-
-
-
- */
 }
