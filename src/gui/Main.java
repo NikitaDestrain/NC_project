0fa60ee3fe894316e516ec730a2b321b3c0cfaec
NikitaDestrain <destra1n1337@gmail.com>
2017-11-29 02:40:15 +0300

    Main избавился от комментариев

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,6 +1,7 @@
 package gui;
 
 import gui.mainform.MainForm;
+import gui.notificationwindow.NotificationForm;
 import properties.Prop;
 import controller.SerializeDeserialize;
 import model.Journal;
@@ -11,48 +12,22 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args)
     {
-
-        try
-        {
+        try {
             Prop prop = new Prop(); //поля этого объекта содержат необхдимые значения
-
-
-
-        } catch (IOException e)
-
-        {
+        } catch (IOException e) {
             System.out.println(e.toString()+" не верно указан файл конфигурации");;
         }
-
-        // todo свернуть в трей
-        // todo иниц файл, иниц генератор на журнал
-        //Journal journal = null;
-          //  journal = new SerializeDeserialize().readJournal();
-
-        //mainform form = new mainform();
-        //taskwindow window = new taskwindow(new mainform());
-        //window.setVisible(true);
-            SwingUtilities.invokeLater(() -> {
-                try {
-                    Journal journal = new SerializeDeserialize().readJournal();
-                    if (journal == null)
-                        JOptionPane.showMessageDialog(null, "Incorrect journal in file. You may create a new one",
-                                "Error", JOptionPane.ERROR_MESSAGE);
-                    new MainForm().setJournal(journal);
-                }
-                catch (IOException e) {
-                    JOptionPane.showMessageDialog(null, "Could not load journal from file. You may create a new one",
-                            "Error", JOptionPane.ERROR_MESSAGE);
-                    new MainForm().setJournal(null);
-                }
-            });
-
-
-
-
-        //TaskForm  e = new TaskForm();
-        //e.setVisible(true);
-
-
+        SwingUtilities.invokeLater(() -> {
+            try {
+                Journal journal = new SerializeDeserialize().readJournal();
+                if (journal == null)
+                    JOptionPane.showMessageDialog(null, "Incorrect journal in file. You may create a new one","Error", JOptionPane.ERROR_MESSAGE);
+                new MainForm().setJournal(journal);
+            }
+            catch (IOException e) {
+                JOptionPane.showMessageDialog(null, "Could not load journal from file. You may create a new one", "Error", JOptionPane.ERROR_MESSAGE);
+                new MainForm().setJournal(null);
+            }
+        });
     }
 }
