12f912b14700216202340ef61cd720e1bf767e3b
NikitaDestrain <destra1n1337@gmail.com>
2017-12-01 22:08:43 +0300

    Исправление мелких ошибок.

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -18,9 +18,12 @@ public class Main {
             try {
                  //поля этого объекта содержат необхдимые значения
                 Journal journal = new SerializeDeserialize().readJournal(ParserProperties.getProperties("PATH_TO_JOURNAL"));
-                if (journal == null)
-                    JOptionPane.showMessageDialog(null, "Incorrect journal in file. You may create a new one","Error", JOptionPane.ERROR_MESSAGE);
-                IDGenerator.getInstance(journal.getMaxId());
+                if (journal == null) {
+                    IDGenerator.getInstance();
+                    JOptionPane.showMessageDialog(null, "Incorrect journal in file. You may create a new one", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+                else
+                    IDGenerator.getInstance(journal.getMaxId());
                 new MainForm().setJournal(journal);
             }
             catch (IOException e) {
