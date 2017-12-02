314cdd5696abe3007cb6036d6b5f0fbd2a038368
AndreyVic <33463480+AndreyVic@users.noreply.github.com>
2017-12-02 18:41:20 +0300

    Update TaskFactory.java
    Удалил не верные комментарии.

--- src/factories/TaskFactory.java
+++ src/factories/TaskFactory.java
@@ -6,17 +6,17 @@ import model.TaskStatus;
 
 import java.util.Date;
 
-public class TaskFactory  //фабрика Task только она должна явно создавать объект Task
+public class TaskFactory  
 
 {
-   //реализовать тут проверку корректности данных?
-    //Так как знаем что задача описыватеся 1 классом ->не абстрактная
+   
+   
    public static Task createTask (String name, TaskStatus status, String description, Date notificationDate, Date plannedDate)
    {
         // todo фаблика должна быть ответственна за конструирование объекта таски. И за проставление у таски ID - тоже
-       return new Task(name, status, description, notificationDate, plannedDate);/// назначить ли id тут? или оставить определение id классу Journal
+       return new Task(name, status, description, notificationDate, plannedDate);// id назначается в контсрукторе Task
 
    }
-//... еще какие-нибудь перегруженные методы
+
 
 }
