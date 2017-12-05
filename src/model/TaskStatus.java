a62b33adbec253b8700fecb5825a632107558f02
AndreyVic <33463480+AndreyVic@users.noreply.github.com>
2017-12-05 18:28:31 +0300

    JavaDoc #13

--- src/model/TaskStatus.java
+++ src/model/TaskStatus.java
@@ -1,5 +1,18 @@
 package model;
 
+/**
+ * possible statuses of the task
+ * <b>Planned,
+ Completed,
+ Rescheduled,
+ Cancelled,
+ Overdue.</b>
+ */
 public enum TaskStatus {
-    Planned, Completed, Rescheduled, Cancelled, Overdue
+
+    Planned,
+    Completed,
+    Rescheduled,
+    Cancelled,
+    Overdue
 }
