3f49ad7dee836707faae5b5ad3d84eb2fb4eb727
AndreyVic <33463480+AndreyVic@users.noreply.github.com>
2017-12-05 19:46:56 +0300

    JavaDoc #13 TaskFactory

--- src/factories/TaskFactory.java
+++ src/factories/TaskFactory.java
@@ -10,8 +10,17 @@ import java.util.Date;
 public class TaskFactory  
 
 {
-   
-   
+
+    /**
+     * Method for creating a task object
+     * {@linkplain model.Task}
+     * @param name task name
+     * @param status task status
+     * @param description task description
+     * @param notificationDate task notification Date
+     * @param plannedDate task plannedDate
+     * @return new object <b> Task </b> with generated id
+     */
    public static Task createTask (String name, TaskStatus status, String description, Date notificationDate, Date plannedDate)
    {
         // todo фаблика должна быть ответственна за конструирование объекта таски. И за проставление у таски ID - тоже
