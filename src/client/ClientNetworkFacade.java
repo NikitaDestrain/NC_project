eb65bffe23003c4ec556f2e42d2dbab7b18c80bf
NikitaDestrain <destra1n1337@gmail.com>
2017-12-19 12:54:26 +0300

    fix

--- src/client/ClientNetworkFacade.java
+++ src/client/ClientNetworkFacade.java
@@ -1,21 +1,28 @@
 package client;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import server.factories.TaskFactory;
+import server.model.Task;
+import server.model.TaskStatus;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class ClientNetworkFacade {
     private static int notificationPort;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, JAXBException {
         System.out.println("Client logs:");
         notificationPort = PortGenerator.getInstance().createPort();
         Socket socket = new Socket("localhost", 1337);
-        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        OutputStream dos = new DataOutputStream(socket.getOutputStream());
         System.out.println("DataOutputStream created");
-        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        InputStream dis = new DataInputStream(socket.getInputStream());
         System.out.println("DataInputStream created");
         System.out.println("Port: " + notificationPort);
         dos.write(notificationPort);
@@ -25,10 +32,17 @@ public class ClientNetworkFacade {
         try (ServerSocket clientServer = new ServerSocket(notificationPort)) {
             Socket client = clientServer.accept();
             System.out.println("Connection accepted.");
-            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+            OutputStream out = new DataOutputStream(client.getOutputStream());
             System.out.println("Notification OutputStream  created");
-            DataInputStream in = new DataInputStream(client.getInputStream());
+            InputStream in = new DataInputStream(client.getInputStream());
             System.out.println("Notification InputStream created");
+
+            //test
+            Task task;
+            JAXBContext context = JAXBContext.newInstance(Task.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            task = (Task) unmarshaller.unmarshal(dis);
+            System.out.println(task);
 
             in.close();
             out.close();
