4b7459bd618ae0c4a07e44fd3b072f8ac8066322
NikitaDestrain <destra1n1337@gmail.com>
2017-12-21 15:30:11 +0300

    Класс Command теперь сериализуется корректно.

--- src/client/commandprocessor/Command.java
+++ src/client/commandprocessor/Command.java
@@ -1,15 +1,22 @@
 package client.commandprocessor;
 
+import client.model.Journal;
+import client.model.Task;
+
 import javax.xml.bind.annotation.*;
 import java.io.Serializable;
 
-@XmlType(name = "command")
+@XmlType(propOrder = {"name", "object"}, name = "Command")
+@XmlRootElement(name = "command")
 @XmlAccessorType(XmlAccessType.FIELD)
+@XmlSeeAlso({Task.class, Journal.class})
 public class Command implements Serializable {
     @XmlElement (name = "name")
     private String name;
     @XmlElement (name = "object")
     private Object object;
+
+    public Command() {}
 
     public Command(String name, Object object) {
         this.name = name;
--- src/server/commandproccessor/Command.java
+++ src/server/commandproccessor/Command.java
@@ -1,16 +1,27 @@
 package server.commandproccessor;
 
+import server.model.Journal;
+import server.model.Task;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
+import javax.xml.bind.annotation.*;
+import java.io.Serializable;
+
 @XmlType(propOrder = {"name", "object"}, name = "Command")
 @XmlRootElement(name = "command")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Command {
+@XmlSeeAlso({Task.class, Journal.class})
+public class Command implements Serializable {
+    @XmlElement (name = "name")
     private String name;
+    @XmlElement (name = "object")
     private Object object;
+
+    public Command() {}
 
     public Command(String name, Object object) {
         this.name = name;
@@ -31,5 +42,12 @@ public class Command {
 
     public void setObject(Object object) {
         this.object = object;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("Command: " + this.getName() + ", Object: " + object.toString());
+        return stringBuffer.toString();
     }
 }
