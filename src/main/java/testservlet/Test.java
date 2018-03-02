e8057b76b19cf480df9b9e2f868d0efd7b38e07f
Artyom Efimov <artyomefimov97@gmail.com>
2018-03-02 10:34:34 +0400

    добавил парсинг xml на страницу tasks.jsp, + туда же добавил форму для отображения параметров сортировки

--- src/main/java/testservlet/Test.java
+++ src/main/java/testservlet/Test.java
@@ -2,6 +2,7 @@ package testservlet;
 
 import server.factories.TaskFactory;
 import server.model.Journal;
+import server.model.JournalContainer;
 import server.model.Task;
 import server.model.TaskStatus;
 
@@ -20,14 +21,27 @@ public class Test {
                 new Date(), new Date(), 0, 0);
         Task task2 = new Task("2", TaskStatus.Overdue, "desc2", new Date(), new Date(),
                 new Date(), new Date(), 1, 1);
-        Journal journal = new Journal();
-        journal.addTask(task1);
-        journal.addTask(task2);
+        Task task3 = new Task("3", TaskStatus.Overdue, "desc3", new Date(), new Date(),
+                new Date(), new Date(), 1, 1);
+        Journal journal1 = new Journal();
+        journal1.addTask(task1);
+        journal1.addTask(task2);
+        journal1.addTask(task3);
+        journal1.setName("j1");
 
-        JAXBContext context = JAXBContext.newInstance(Journal.class);
+        Journal journal2 = new Journal();
+        journal2.addTask(task2);
+        journal2.addTask(task1);
+        journal2.setDescription("j1Desc");
+
+        JournalContainer container = new JournalContainer();
+        container.addJournal(journal1);
+        container.addJournal(journal2);
+
+        JAXBContext context = JAXBContext.newInstance(JournalContainer.class);
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
-        marshaller.marshal(journal, new File("xsd/journal.xml"));
+        marshaller.marshal(container, new File("xsd/journals.xml"));
     }
 }
